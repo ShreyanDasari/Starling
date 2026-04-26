8de4b91d776bb0178ce131e33ad9e0166e118bc6
ShreyanDasari <shreyandasari007@gmail.com>
2026-04-26 16:28:42 +0530

    Added bootstrap node

--- Starling Frontend/src/App.tsx
+++ Starling Frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import ChatInterface from './pages/ChatInterface'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
